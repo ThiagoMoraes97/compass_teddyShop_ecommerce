3b4be683f9075a235d4911bcbe2c8fa3702d014c
Thiago Moraes <thiagomottamoraes@gmail.com>
2025-03-18 14:30:33 -0300

    Desktop slider cards completed.

--- js/index.js
+++ js/index.js
@@ -7,6 +7,13 @@ const categoryButton = document.querySelector('.open_options');
 const categoryOptions = document.getElementById('options');
 const emailInput = document.getElementById('email');
 const verifyEmailForm = document.getElementById('verify_email');
+const slider = document.querySelector('.services-cards-wrapper');
+const sliderCards = document.querySelectorAll('.services-card');
+const sliderDots = document.querySelector('.slide_dots');
+const feedbackSlider = document.querySelector('.feedback-cards-wrapper');
+const feedbackCards = document.querySelectorAll('.feedback-card');
+const feedbackSliderDots = document.querySelector('.feedback_slide_dots');
+
 
 // Abrir menu mobile
 mobileMenuOpenBtn.addEventListener('click', () => {
@@ -23,12 +30,12 @@ categoryButton.addEventListener('click', () => {
     console.log('clicou');
 });
 
+
 verifyEmailForm.onsubmit = verifyEmail;
 
 // Função para atualizar classes com base no tamanho da tela
 function uploadElementClasses() {
     const windowWidth = window.innerWidth;
-    console.log(windowWidth);
 
     if (windowWidth <= 768) {
         desktopLinks.classList.add('desktop_only');
@@ -56,6 +63,129 @@ function verifyEmail(event) {
     emailInput.value = '';
 }
 
-// Executa ao carregar e ao redimensionar
+function generateSliderDots() {
+ 
+    let numberOfSliderDots = (sliderCards.length) - 2;
+    let index;
+
+    for (let i = 0; i < numberOfSliderDots; i++) {
+        index = i;
+        const dot = document.createElement('span');
+        dot.classList.add('dot');
+        dot.classList.add(`index-${index}`);
+        if (i === 0) {
+            dot.classList.add('active');
+        }
+        sliderDots.appendChild(dot);
+    }
+}
+
+function generateFeedbackSliderDots() {
+    let numberOfSliderDots = (feedbackCards.length) - 2;
+    let index;
+
+    for (let i = 0; i < numberOfSliderDots; i++) {
+        index = i;
+        const dot = document.createElement('span');
+        dot.classList.add('feedback_dot');
+        dot.classList.add(`feedback-index-${index}`);
+        if (i === 0) {
+            dot.classList.add('active');
+        }
+        feedbackSliderDots.appendChild(dot);
+    }
+}
+
+let currentIndex = 0;
+let currentIndexFeedback = 0;
+
+function moveSliderCards(index) {
+    
+    if (currentIndex === index) {
+        return;
+    }
+
+    const cardWidth = sliderCards[0].offsetWidth + 48; 
+    
+    if (currentIndex < index) {
+        slider.scrollLeft += cardWidth * (index - currentIndex); 
+    } else {
+        slider.scrollLeft -= cardWidth * (currentIndex - index); 
+    }
+   
+    currentIndex = index;
+}
+
+function updateCurrentDot(index) {
+    let currentDot = document.querySelector('.dot.active');
+
+    if (currentDot) {
+        currentDot.classList.remove('active');
+    }
+
+    let newCurrentDot = document.querySelector(`.index-${index}`);
+
+    if (!newCurrentDot) {
+        return
+    }
+
+    newCurrentDot.classList.add('active');
+}
+
+function moveFeedbackSliderCards(index) {
+    if (currentIndexFeedback === index) return;
+
+    const cardWidth = feedbackCards[0].offsetWidth + 48;
+
+    if (currentIndexFeedback < index) {
+        feedbackSlider.scrollLeft += cardWidth * (index - currentIndexFeedback); 
+    } else {
+        feedbackSlider.scrollLeft -= cardWidth * (currentIndexFeedback - index); 
+    }
+   
+    currentIndexFeedback = index;
+}
+
+function updateCurrentFeedbackDot(index) {
+    let currentDot = document.querySelector('.feedback_dot.active');
+
+    if (currentDot) {
+        currentDot.classList.remove('active');
+    }
+
+    let newCurrentDot = document.querySelector(`.feedback-index-${index}`);
+
+    if (!newCurrentDot) {
+        return
+    }
+
+    newCurrentDot.classList.add('active');
+}
+
+generateSliderDots();
+generateFeedbackSliderDots();
+
+document.addEventListener('click', function(event) {
+    if (event.target.classList.contains('dot')) {
+        console.log(event.target);
+        event.stopPropagation();
+        const index = parseInt(event.target.classList[1].split('-')[1]);
+        moveSliderCards(index);
+        updateCurrentDot(index);
+    }
+});
+
+document.addEventListener('click', function(event) {
+    if (event.target.classList.contains('feedback_dot')) {
+        console.log(event.target);
+        event.stopPropagation();
+        const index = parseInt(event.target.classList[1].split('-')[2]);
+        moveFeedbackSliderCards(index);
+        updateCurrentFeedbackDot(index);
+    }
+});
+
+
+
 uploadElementClasses();
 window.addEventListener('resize', uploadElementClasses);
