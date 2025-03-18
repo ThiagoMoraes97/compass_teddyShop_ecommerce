e0b4636b3a64bea55e97ad69570a54259a74d412
Thiago Moraes <thiagomottamoraes@gmail.com>
2025-03-18 16:36:46 -0300

    Some final adjustments.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Seleção de elementos
 const mobileMenu = document.querySelector('.mobile_menu');
 const mobileMenuOpenBtn = document.querySelector('.menu-button-open');
 const mobileMenuCloseBtn = document.querySelector('.menu-button-close');
@@ -14,26 +15,39 @@ const feedbackSlider = document.querySelector('.feedback-cards-wrapper');
 const feedbackCards = document.querySelectorAll('.feedback-card');
 const feedbackSliderDots = document.querySelector('.feedback_slide_dots');
 
+// Estados globais
+let currentIndex = 0;
+let currentIndexFeedback = 0;
 
-// Abrir menu mobile
-mobileMenuOpenBtn.addEventListener('click', () => {
-    mobileMenu.classList.add('open');
-});
+// Event listeners
+mobileMenuOpenBtn.addEventListener('click', () => mobileMenu.classList.add('open'));
 
-// Fechar menu mobile
-mobileMenuCloseBtn.addEventListener('click', () => {
-    mobileMenu.classList.remove('open');
-});
+mobileMenuCloseBtn.addEventListener('click', () => mobileMenu.classList.remove('open'));
 
 categoryButton.addEventListener('click', () => {
     categoryOptions.classList.toggle('open');
     console.log('clicou');
 });
 
-
 verifyEmailForm.onsubmit = verifyEmail;
 
-// Função para atualizar classes com base no tamanho da tela
+document.addEventListener('click', (event) => {
+    if (event.target.classList.contains('dot')) {
+        const index = parseInt(event.target.classList[1].split('-')[1]);
+        moveSliderCards(index);
+        updateCurrentDot(index);
+    }
+
+    if (event.target.classList.contains('feedback_dot')) {
+        const index = parseInt(event.target.classList[1].split('-')[2]);
+        moveFeedbackSliderCards(index);
+        updateCurrentFeedbackDot(index);
+    }
+});
+
+window.addEventListener('resize', uploadElementClasses);
+
+// Funções
 function uploadElementClasses() {
     const windowWidth = window.innerWidth;
 
@@ -54,170 +68,76 @@ function verifyEmail(event) {
     event.preventDefault();
     const email = emailInput.value;
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
     if (emailRegex.test(email)) {
-        verifyEmailForm.classList.remove("invalid");
-        alert('Email enviado com sucesso!'); 
+        verifyEmailForm.classList.remove('invalid');
+        alert('Email enviado com sucesso!');
     } else {
-        verifyEmailForm.classList.add("invalid"); 
+        verifyEmailForm.classList.add('invalid');
     }
 
     emailInput.value = '';
 }
 
 function generateSliderDots() {
-
     const windowWidth = window.innerWidth;
-    let numberOfSliderDots;
-
-    if (windowWidth <= 768) {
-        numberOfSliderDots = (sliderCards.length);
-    } else {
-        numberOfSliderDots = (sliderCards.length) - 2;
-    }
- 
-    let index;
+    const numberOfSliderDots = windowWidth <= 768 ? sliderCards.length : sliderCards.length - 2;
 
     for (let i = 0; i < numberOfSliderDots; i++) {
-        index = i;
         const dot = document.createElement('span');
-        dot.classList.add('dot');
-        dot.classList.add(`index-${index}`);
-        if (i === 0) {
-            dot.classList.add('active');
-        }
+        dot.classList.add('dot', `index-${i}`);
+        if (i === 0) dot.classList.add('active');
         sliderDots.appendChild(dot);
     }
 }
 
 function generateFeedbackSliderDots() {
     const windowWidth = window.innerWidth;
-    let numberOfSliderDots;
-
-    if (windowWidth <= 768) {
-        numberOfSliderDots = (sliderCards.length);
-    } else {
-        numberOfSliderDots = (sliderCards.length) - 2;
-    }
-    
-    let index;
+    const numberOfSliderDots = windowWidth <= 768 ? sliderCards.length : sliderCards.length - 2;
 
     for (let i = 0; i < numberOfSliderDots; i++) {
-        index = i;
         const dot = document.createElement('span');
-        dot.classList.add('feedback_dot');
-        dot.classList.add(`feedback-index-${index}`);
-        if (i === 0) {
-            dot.classList.add('active');
-        }
+        dot.classList.add('feedback_dot', `feedback-index-${i}`);
+        if (i === 0) dot.classList.add('active');
         feedbackSliderDots.appendChild(dot);
     }
 }
 
-let currentIndex = 0;
-let currentIndexFeedback = 0;
-
 function moveSliderCards(index) {
-    
-    if (currentIndex === index) {
-        return;
-    }
+    if (currentIndex === index) return;
 
-    const windowWidth = window.innerWidth;
-    let cardWidth;
+    const cardWidth = window.innerWidth <= 768 ? sliderCards[0].offsetWidth + 65 : sliderCards[0].offsetWidth + 48;
 
-    if (windowWidth <= 768) {
-        cardWidth = sliderCards[0].offsetWidth + 65;
-    } else {
-        cardWidth = sliderCards[0].offsetWidth + 48; 
-    }
-
-    
-    if (currentIndex < index) {
-        slider.scrollLeft += cardWidth * (index - currentIndex); 
-    } else {
-        slider.scrollLeft -= cardWidth * (currentIndex - index); 
-    }
-   
+    slider.scrollLeft += cardWidth * (index - currentIndex);
     currentIndex = index;
 }
 
 function updateCurrentDot(index) {
-    let currentDot = document.querySelector('.dot.active');
+    const currentDot = document.querySelector('.dot.active');
+    if (currentDot) currentDot.classList.remove('active');
 
-    if (currentDot) {
-        currentDot.classList.remove('active');
-    }
-
-    let newCurrentDot = document.querySelector(`.index-${index}`);
-
-    if (!newCurrentDot) {
-        return
-    }
-
-    newCurrentDot.classList.add('active');
+    const newCurrentDot = document.querySelector(`.index-${index}`);
+    if (newCurrentDot) newCurrentDot.classList.add('active');
 }
 
 function moveFeedbackSliderCards(index) {
     if (currentIndexFeedback === index) return;
 
-    const windowWidth = window.innerWidth;
-    let cardWidth;
+    const cardWidth = window.innerWidth <= 768 ? sliderCards[0].offsetWidth + 70 : sliderCards[0].offsetWidth + 48;
 
-    if (windowWidth <= 768) {
-        cardWidth = sliderCards[0].offsetWidth + 70;
-    } else {
-        cardWidth = sliderCards[0].offsetWidth + 48; 
-    }
-
-    if (currentIndexFeedback < index) {
-        feedbackSlider.scrollLeft += cardWidth * (index - currentIndexFeedback); 
-    } else {
-        feedbackSlider.scrollLeft -= cardWidth * (currentIndexFeedback - index); 
-    }
-   
+    feedbackSlider.scrollLeft += cardWidth * (index - currentIndexFeedback);
     currentIndexFeedback = index;
 }
 
 function updateCurrentFeedbackDot(index) {
-    let currentDot = document.querySelector('.feedback_dot.active');
+    const currentDot = document.querySelector('.feedback_dot.active');
+    if (currentDot) currentDot.classList.remove('active');
 
-    if (currentDot) {
-        currentDot.classList.remove('active');
-    }
-
-    let newCurrentDot = document.querySelector(`.feedback-index-${index}`);
-
-    if (!newCurrentDot) {
-        return
-    }
-
-    newCurrentDot.classList.add('active');
+    const newCurrentDot = document.querySelector(`.feedback-index-${index}`);
+    if (newCurrentDot) newCurrentDot.classList.add('active');
 }
 
+// Inicialização
 generateSliderDots();
 generateFeedbackSliderDots();
-
-document.addEventListener('click', function(event) {
-    if (event.target.classList.contains('dot')) {
-        console.log(event.target);
-        event.stopPropagation();
-        const index = parseInt(event.target.classList[1].split('-')[1]);
-        moveSliderCards(index);
-        updateCurrentDot(index);
-    }
-});
-
-document.addEventListener('click', function(event) {
-    if (event.target.classList.contains('feedback_dot')) {
-        console.log(event.target);
-        event.stopPropagation();
-        const index = parseInt(event.target.classList[1].split('-')[2]);
-        moveFeedbackSliderCards(index);
-        updateCurrentFeedbackDot(index);
-    }
-});
-
-
-
 uploadElementClasses();
-window.addEventListener('resize', uploadElementClasses);
